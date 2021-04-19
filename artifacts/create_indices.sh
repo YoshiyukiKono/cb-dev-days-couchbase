7a17ebe14ae96e97b00cea3d007c11ba5001944e
Yoshiyuki Kono <yoshiyuki.kono@gmail.com>
2021-04-19 18:26:42 +0900

    Update create_indices.sh

--- artifacts/create_indices.sh
+++ artifacts/create_indices.sh
@@ -1,4 +1,7 @@
-CREATE INDEX idx_doc_type ON `{{BUCKET}}`(doc.`type`);
-CREATE INDEX idx_docType ON `{{BUCKET}}`(docType);
-CREATE INDEX idx_customer_basic ON `{{BUCKET}}`(username, custId, (custName.firstName), (custName.lastname)) WHERE doc.`type`="customer";
-CREATE INDEX idx_user_pw on `{{BUCKET}}`(username, userId, `password`) WHERE docType="user";
+CREATE INDEX idx_doc_type ON `retailsample`(doc.`type`);
+CREATE INDEX idx_docType ON `retailsample`(docType);
+CREATE INDEX idx_customer_basic ON `retailsample`(username, custId, (custName.firstName), (custName.lastname)) WHERE doc.`type`="customer";
+CREATE INDEX idx_user_pw on `retailsample`(username, userId, `password`) WHERE docType="user";
+
+
+cbq -e couchbase://localhost -u {{USER}} -p {{PASS}} -f /tmp/cbdevdays_indexes.txt
